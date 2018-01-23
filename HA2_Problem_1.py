515911811b939a9fb403b7f096f85d321d86d49c
TonyShony <ashiian@nes.ru>
2018-01-24 00:08:19 +0300

    test

--- HA2_Problem_1.py
+++
@@ -1,9 +0,0 @@
-def sumThreeFive(n):
-    a = 0
-    for i in range(n):
-        if i % 3 == 0 or i % 5 == 0:
-            a += i
-    return a
-
-
-print(sumThreeFive(int(input())))
--- HA2_Problem_2.py
+++
@@ -1,9 +0,0 @@
-def fibo(n):
-    def inFibo(a, b, c):
-        if c == 1:
-            return b
-        return inFibo(a + b, a, c - 1)
-    return inFibo(2, 1, n)
-
-
-print(fibo(int(input())))
